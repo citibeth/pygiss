4789a0786e8ebe70ae8c2407763be0ecc1c09dc7
Bob Fischer <rpf2116@columbia.edu>
2013-09-22 11:46:18 -0400

    Added license

--- test/plottest3.py
+++ test/plottest3.py
@@ -1,3 +1,19 @@
+# pyGISS: GISS Python Library
+# Copyright (c) 2013 by Robert Fischer
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # Demonstrates various customization features for plotting
 # This tries to be a "kitchen sink" of the kinds of customizations you can
 # do for one plot, while still using giss.plot.plot_var().
