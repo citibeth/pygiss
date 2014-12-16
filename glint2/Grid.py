db159f4a355f2d53fb9821c38464c2f720f0e1b8
citibob <robert.fischer@nasa.gov>
2014-12-15 22:03:32 -0500

    Bug fixes:  1. Change of method name  2. Glint2 code should not be concerned with the extra elevation point added by ModelE (which is at a higher level than Glint2).

--- glint2/Grid.py
+++ glint2/Grid.py
@@ -214,7 +214,7 @@ class Plotter1h :
 	def __init__(self, glint2_config, ice_sheet, mmaker=None) :
 		if mmaker is None :
 			mmaker = glint2.MatrixMaker(glint2_config)
-		self.mat_1h_to_2 = mmaker.hp_to_ice(ice_sheet)
+		self.mat_1h_to_2 = mmaker.hp_to_iceinterp(ice_sheet)
 
 		nc = netCDF4.Dataset(glint2_config)
 		self.plotter2 = Plotter2(nc=nc, vname='m.' + ice_sheet + '.grid2')
@@ -224,7 +224,7 @@ class Plotter1h :
 	def pcolormesh(self, mymap, val1h, **plotargs) :
 #		val1h = val1h.reshape(-1)
 		print val1h.shape
-		val1h = val1h[1:,:,:].reshape(-1)	# Discard height point for non-model ice
+		val1h = val1h.reshape(-1)
 		print val1h.shape
 
 		val2 = glint2.coo_multiply(self.mat_1h_to_2, val1h, fill=np.nan, ignore_nan=False)	# Make np.nan
