7fec40c11313f80145d894f9e6c65b0d0967cf68
Bob <robert.fischer@nasa.gov>
2015-11-17 16:21:13 -0500

    Added missing files.

---
+++ memoize.py
@@ -0,0 +1,17 @@
+# https://pypi.python.org/pypi/memoize/
+
+from functools import wraps
+
+
+def mproperty(fn):
+    attribute = "_memo_%s" % fn.__name__
+
+    @property
+    @wraps(fn)
+    def _property(self):
+        if not hasattr(self, attribute):
+            setattr(self, attribute, fn(self))
+        return getattr(self, attribute)
+
+    return _property
+
