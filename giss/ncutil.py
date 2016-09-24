307ca1ceb2e1c256fdc02da48dd07f7afbebfcfa
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2016-09-23 23:53:04 -0400

    Added netCDF diff utility

--- giss/ncutil.py
+++ giss/ncutil.py
@@ -14,6 +14,9 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import numpy as np
+import netCDF4
+import sys
 
 # Copy a netCDF file (so we can add more stuff to it)
 class copy_nc(object):
@@ -44,7 +47,7 @@ class copy_nc(object):
         self.avoid_vars.add(var_name)
         return self.ncout.createVariable(var_name, *args, **kwargs)
 
-    def define_vars(self):
+    def define_vars(self, **kwargs):
         self.vars = self.nc0.variables.keys()
 
         # Figure out which dimensions to copy
@@ -69,7 +72,7 @@ class copy_nc(object):
             if var_name in self.avoid_vars : continue
 
             var = self.nc0.variables[var_name]
-            varout = self.ncout.createVariable(ovname, var.dtype, var.dimensions)
+            varout = self.ncout.createVariable(ovname, var.dtype, var.dimensions, **kwargs)
             for aname, aval in var.__dict__.items():
                 if not self.attrib_filter(aname) : continue
                 varout.setncattr(aname, aval)
@@ -85,3 +88,81 @@ class copy_nc(object):
             ovar = self.ncout.variables[ovname]
             ovar[:] = ivar[:]
 
+def default_diff_fn(var, val0, val1):
+    """Called when we see a difference"""
+    pass
+
+def diff(nc0, nc1, ncout=None,
+        var_filter=lambda x : x,
+        rtol=None, atol=None, equal_nan=None,    # np.isclose()
+         **kwargs):    # nc.createVariable()
+    """Finds differences between two NetCDF files.
+    Optional args: rtol, atol, equal_nan.  See nump.isclose()"""
+
+    isclose_kwargs = dict()
+    if rtol is not None:
+        isclose_kwargs['rtol'] = rtol
+    if atol is not None:
+        isclose_kwargs['atol'] = atol
+    if equal_nan is not None:
+        isclose_kwargs['equal_nan'] = equal_nan
+
+
+    opened = list()
+
+    try:
+        if not isinstance(nc0, netCDF4.Dataset):
+            nc0 = netCDF4.Dataset(nc0, 'r')
+            opened.append(nc0)
+
+        if not isinstance(nc1, netCDF4.Dataset):
+            nc1 = netCDF4.Dataset(nc1, 'r')
+            opened.append(nc1)
+
+        if ncout is not None:
+            if not isinstance(ncout, netCDF4.Dataset):
+                ncout = netCDF4.Dataset(ncout, 'w', clobber=True)
+                opened.append(ncout)
+
+        extra0 = list()
+        extra1 = list()
+        diffs = list()
+
+        remain1 = set([var for var in nc1.variables if var_filter(var) is not None])
+        vars0 = [var for var in nc0.variables if var_filter(var) is not None]
+
+        for var in vars0:
+            if var not in remain1:
+                extra0.append(var)
+            else:
+                val0 = nc0.variables[var][:]
+                val1 = nc1.variables[var][:]
+                if not np.isclose(val0, val1, **isclose_kwargs).all():
+                    diffs.append(var)
+
+            remain1.remove(var)
+
+        for var in remain1:
+            extra1.append(var)
+
+        # Write out if we're given an output file
+        diffs_set = set(diffs)
+        if ncout is not None:
+            nccopy = copy_nc(nc0, ncout,
+                var_filter=lambda x : x if x in diffs_set else None)
+            nccopy.define_vars(**kwargs)
+
+            for var in diffs:
+                val0 = nc0.variables[var][:]
+                val1 = nc1.variables[var][:]
+                ncout[var][:] = val1 - val0
+
+
+        return (extra0, extra1, diffs)
+
+    finally:
+        for nc in opened:
+            try:
+                nc.close()
+            except Exception as e:
+                sys.stderr.write('Exception in ncutil.py diff(): {}\n'.format(e))
