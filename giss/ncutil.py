89a0c4f6c67a239b1abacb5067b6c6ded6049403
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2016-11-01 22:13:51 -0400

    ncutil.py:
    1. install_nc(): print to STDOUT when installing files
    
    2. Fix bug: Only do netCDF missing value substitution for floating point
    
    3. Bug fixes in fetching
    
    4. sum_fetch1(): do np.sum() on an annotated fetch record (lift level 1)

--- giss/ncutil.py
+++ giss/ncutil.py
@@ -209,7 +209,7 @@ def install_nc(ifname, odir, installed=None):
     # Copy the netCDF file to the destination
     _,ifleaf = os.path.split(ifname)
     ofname = os.path.join(odir, ifleaf)
-    print('{} ->\n    {}'.format(ifname, ofname))
+    print('Installing {} ->\n    {}'.format(ifname, ofname))
     shutil.copyfile(ifname, ofname)
 
     # Install dependencies of this file
@@ -260,15 +260,19 @@ def ncdata(fname, var_name, *index, nan=np.nan, missing_value=None, missing_thre
     var = nc.variables[var_name]
 
     data = var[index]
-    if missing_value is not None:
-        # User override of NetCDF standard
-        data[data == missing_value] = nan
-    elif hasattr(var, 'missing_value'):
-        # NetCDF standard
-        data[data == var.missing_value] = nan
-    elif missing_threshold is not None:
-        # Last attempt to fix a broken file
-        data[np.abs(data) > missing_threshold] = nan
+
+    if not np.issubdtype(var.dtype, np.integer):
+        # Missing value stuff only makes sense for floating point
+
+        if missing_value is not None:
+            # User override of NetCDF standard
+            data[data == missing_value] = nan
+        elif hasattr(var, 'missing_value'):
+            # NetCDF standard
+            data[data == var.missing_value] = nan
+        elif missing_threshold is not None:
+            # Last attempt to fix a broken file
+            data[np.abs(data) > missing_threshold] = nan
 
     return data
 
@@ -277,22 +281,24 @@ def _fetch_shape(var, index):
     """Given a variable and indexing, determines the shape of the
     resulting fetch (without actually doing it)."""
     shape = []
-    oshape = []
-#    for var_shape,ix in zip(var.shape,index):
+    dims = []
     for i in range(0,len(var.shape)):
         if i >= len(index):    # Implied ':' for this dim
+            dims.append(var.dimensions[i])
             shape.append(var.shape[i])
         else:
             ix = index[i]
             if isinstance(ix, slice):
+                dims.append(var.dimensions[i])
                 start = 0 if ix.start is None else ix.start
-                stop = var_shape[i] if ix.stop is None else ix.stop
+                stop = var.shape[i] if ix.stop is None else ix.stop
                 step = 1 if ix.step is None else ix.step
                 shape.append((stop - start) // step)
-    print('******************', var.shape, index, shape)
-    return tuple(shape)            
+    return tuple(shape), tuple(dims)
 
-FetchTuple = namedtuplex('FetchTuple', ('attrs', 'data'))
+FetchTuple = xnamedtuple('FetchTuple', ('attrs', 'data'))
+
+
 
 @function()
 def ncattrs(file_name, var_name):
@@ -336,8 +342,9 @@ def add_fetch_attrs(attrs, file_name, var_name, *index, nan=np.nan, missing_valu
     attrs[('fetch', 'missing_threshold')] = missing_threshold
     attrs[('fetch', 'nan')] = np.nan
     attrs[('fetch', 'index')] = index
-    fetch_shape = _fetch_shape(var, index)
+    fetch_shape,fetch_dims = _fetch_shape(var, index)
     attrs[('fetch', 'shape')] = fetch_shape
+    attrs[('fetch', 'dimensions')] = fetch_dims
     attrs[('fetch', 'size')] = functools.reduce(operator.mul, fetch_shape, 1)
     attrs[('fetch', 'dtype')] = attrs[('var', 'dtype')]
 
@@ -348,11 +355,56 @@ def ncfetch(file_name, var_name, *index, nan=np.nan, missing_value=None, missing
     attrsW = ncattrs(file_name, var_name)
     add_fetch_attrs(attrsW(), file_name, var_name, **kwargs)
 
-    return FetchTuple(attrsW,
-        bind(ncdata, file_name, var_name, *index, **kwargs))
+    return FetchTuple(
+        attrsW,
+            bind(ncdata, file_name, var_name, *index, **kwargs))
+# ---------------------------------------------------------
+@function()
+def sum_fetch1(fetch, axis=None, dtype=None, out=None, keepdims=False):
+    """Works like np.sum, but on a fetch record..."""
+    attrs0 = fetch.attrs()
+    data1 = fetch.data
 
-#ncfetch = FetchTuple(ncattrs, ncdata)
-# -------------------------------------------------
+    ashape = attrs0[('fetch', 'shape')]
+    adims = attrs0[('fetch', 'dimensions')]
+
+    # https://docs.scipy.org/doc/numpy-1.10.1/reference/generated/numpy.sum.html
+    # axis : None or int or tuple of ints, optional
+    # 
+    # Axis or axes along which a sum is performed. The default (axis =
+    # None) is perform a sum over all the dimensions of the input
+    # array. axis may be negative, in which case it counts from the
+    # last to the first axis.
+    #
+    # If this is a tuple of ints, a sum is performed on multiple axes,
+    # instead of a single axis or all the axes as before.
+    if axis is None:
+        axes = set(range(len(ashape)))
+    if isinstance(axis, tuple):
+        axes = set(axis)
+    else:
+        axes = set((axis,))
+
+    shape = []
+    dims = []
+    for i in range(0,len(ashape)):
+        if i in axes:
+            if keepdims:
+                shape.append(1)
+                dims.append(adims[i])
+        else:
+            shape.append(ashape[i])
+            dims.append(adims[i])
+
+    attrs0[('fetch', 'shape')] = tuple(shape)
+    attrs0[('fetch', 'dimensions')] = tuple(dims)
+
+    data = functional.lift_once(np.sum, fetch.data, axis=axis, dtype=dtype, out=out, keepdims=keepdims)
+
+    return FetchTuple(fetch.attrs, data)
+
+sum_fetch2 = functional.lift()(sum_fetch1)
+# ---------------------------------------------------------
 _zero_one = np.array([0.,1.])
 
 def convert_unitsV(fetch, ounits):
