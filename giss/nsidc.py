7ed08837c488415583654e437b610fef90dfc9d0
Elizabeth Fischer <elizabeth.fischer@alaska.edu>
2020-04-29 10:38:18 -0800

    Converted glaciers and nsidc stuff to make.py

--- giss/nsidc.py
+++ giss/nsidc.py
@@ -37,7 +37,7 @@ reNSIDC_0481 = re.compile(r'(TSX|TDX)_([EWS][0-9.]+[NS])_(\d\d(Jan|Feb|Mar|Apr|M
 
 def parse_0481(path):
     """
-    See: https://nsidc.org/data/nsidc-0481"""
+    See: https://org/data/nsidc-0481"""
 
     dir,leaf = os.path.split(path)
 
@@ -65,57 +65,122 @@ def parse_0481(path):
     return ret
 
 # -------------------------------------------------------------
-def tiff_to_netcdf(pfile, odir, all_files=None, oext='.nc', reftime='2008-01-01'):
-    """Converts single GeoTIFF to NetCDF
-    ifname:
-        The input GeoTIFF file
-    oext:
-        Extension to use on the output file
-    Returns:
-        Name of the output NetCDF file (as a pfile parsed file)"""
+# ---------------------------------------------------
+class tiff_to_netcdf0(object):
+    """Makefile macro, convert a GeoTIFF to raw NetCDF file.
+    Generally used only from tiff_to_netcdf().
+    pfile:
+        The iopfile.PFile to convert
+    odir:
+        Directory to place the output"""
 
-    os.makedirs(odir, exist_ok=True)
+    def __init__(self, makefile, pfile, odir):
 
-    # Generate ofname
-    opfile = type(pfile)(pfile.items())
-    opfile['dir'] = odir
-    opfile['ext'] = oext
-    tmp0 = os.path.join(odir, opfile.format(ext='.tiff_to_netcdf_0.nc'))
+        self.ipath = pfile.path
+        self.opath = os.path.join(odir, pfile.format(
+            dir=odir,
+            ext='.tiff_to_netcdf_0.nc'))
+        self.rule = makefile.add(self.run,
+            (self.ipath,),
+            (self.opath,))
 
-    # Don't regenerate files already built
-    if not ioutil.needs_regen((opfile.path,), (pfile.path,)):
-        return opfile
+    def run(self):
+        print('opath ',self.opath)
+        os.makedirs(os.path.split(self.opath)[0], exist_ok=True)
 
-    try:
-        print("Converting {} to {}".format(pfile.path, opfile.path))
+        print("Converting {} to {}".format(self.ipath, self.opath))
         # use gdal's python binging to convert GeoTiff to netCDF
         # advantage of GDAL: it gets the projection information right
         # disadvantage: the variable is named "Band1", lacks metadata
-        ds = gdal.Open(pfile.path)
-        ds = gdal.Translate(tmp0, ds)
+        ds = gdal.Open(self.ipath)
+        ds = gdal.Translate(self.opath, ds)
         ds = None
+# -------------------------------------------------------------------------
+class tiff_to_netcdf(object):
+    """Makefile macro, convert a GeoTIFF to NetCDF file with fixed metadata.
+    pfile:
+        The iopfile.PFile to convert
+    odir:
+        Directory to place the output
+    reftime:    
+        Reference time for CF Times in the file
+    """
+
+    def __init__(self, makefile, pfile, odir, reftime='2008-01-01'):
+
+        sub = tiff_to_netcdf0(makefile, pfile, odir)
 
         # This deduces the mid-point (nominal) date from the filename
-        nominal_date = pfile['startdate'] + (pfile['enddate'] - pfile['startdate']) / 2
+        self.nominal_date = pfile['startdate'] + (pfile['enddate'] - pfile['startdate']) / 2
+        self.reftime = reftime        
+        self.parameter = pfile['parameter']
+        self.ipath = sub.opath
+        self.opath = os.path.join(odir, pfile.format(
+            dir=odir,
+            ext='.nc'))
 
+        self.rule = makefile.add(self.run, (self.ipath,), (self.opath,))
+
+    def run(self):
         # Set the time axis
-        var = pfile['parameter']
+        parameter = self.parameter
+        reftime = self.reftime
         inputs = [
-            f'-setreftime,{reftime}',
-            f'-setattribute,{var}@units="m year-1"',
-            f'-chname,Band1,{var}',
-            f'{tmp0}']
+            '-setreftime,{}'.format(self.reftime),
+            '-setattribute,{}@units="m year-1"'.format(self.parameter),
+            '-chname,Band1,{}'.format(self.parameter),
+            '{}'.format(self.ipath)]
         cdo.settaxis(
-            nominal_date.isoformat(),
+            self.nominal_date.isoformat(),
             input=' '.join(inputs),
-            output=opfile.path,
+            output=self.opath,
             options="-f nc4 -z zip_2")
+# -------------------------------------------------------------------------
+class tiffs_to_netcdfs(object):
+    """Makefile macro, convert a directory full of GeoTIFFs to NetCDF.
+    idir:
+        Input directory of GeoTIFF files
+    odir:
+        Directory for output files
+    filter_attrs:
+        Files converted must match these attributes.
+        For example: source, grid, parameter
+    reftime:    
+        Reference time for CF Times in the file
+    blacklist:
+        Files to NOT include
+    max_files:
+        Maximum number of files to include
+    """
 
-        if all_files is not None:
-            all_files.append(opfile.path)
-        return opfile
-    finally:
-        try:
-            os.remove(tmp0)
-        except FileNotFoundError:
-            pass
+    def __init__(
+        self, makefile, idir, odir,
+        reftime='2008-01-01', blacklist=set(), max_files=10000000,
+        filter_attrs=dict()):
+
+        self.blacklist = blacklist
+
+        # Get list of .tif files
+        attrs = dict(filter_attrs.items())
+        attrs['ext'] = '.tif'
+        filter_fn = iopfile.filter_attrs(attrs)
+        self.pfiles_tif = iopfile.listdir(idir, parse_0481, filter_fn)
+
+        # Go through each file
+        inputs = list()
+        outputs = list()
+        for ix,pf in enumerate(self.pfiles_tif):
+
+            # Don't iterate over blacklisted items
+            if pf.leaf in self.blacklist:
+                continue
+
+            # Cut it off early
+            if ix >= max_files:
+                break
+
+            rule = tiff_to_netcdf(makefile, pf, odir, reftime=reftime).rule
+            inputs += rule.inputs
+            outputs += rule.outputs
+
+        self.rule = makefile.add(None, inputs, outputs)
